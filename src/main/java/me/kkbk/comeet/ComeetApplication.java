7d315a752488b4713d513cca61106ca025f8f633
ys <ys@ys-MacBookAir.local>
2023-05-14 21:33:12 +0900

    init

---
+++ src/main/java/me/kkbk/comeet/ComeetApplication.java
@@ -0,0 +1,13 @@
+package me.kkbk.comeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kkbk/comeet/ComeetApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kkbk.comeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
