50d8dc343d01c15dcceb179ce618a75de98f9548
kkambbak <yunsung9841@gmail.com>
2023-05-17 00:40:32 +0900

    add CrossOrigin

--- src/main/java/me/kkbk/comeet/member/MemberController.java
+++ src/main/java/me/kkbk/comeet/member/MemberController.java
@@ -1,12 +1,14 @@
 package me.kkbk.comeet.member;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MemberController {
 
     private MemberDaoService service;
